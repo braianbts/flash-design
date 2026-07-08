030335083d249a65cff40914a94e18f8f82627b9
braian barrientos <braianbts@gmail.com>
2026-07-07 23:05:19 -0300

    asd

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,6 @@ const links = [
   { href: "/", label: "Inicio" },
   { href: "/#new-arrivals", label: "Productos" },
   { href: "/#destacados", label: "Destacados" },
-  { href: "/#nosotros", label: "Nosotros" },
 ];
 
 export default function Navbar() {
